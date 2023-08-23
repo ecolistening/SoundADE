12b5417264da90bf9821caf27e5679d6507f09ab
David Kadish <david.kadish@gmail.com>
2023-08-23 11:08:41 +0200

    Dataset definition and testing data for WA

--- src/soundade/data/datasets.py
+++ src/soundade/data/datasets.py
@@ -624,9 +624,61 @@ class Cairngorms(Dataset):
         return ddf
 
 
+class WildlifeAcoustics(Dataset):
+    @staticmethod
+    def extract_features(b: db.Bag, frame: int, hop: int, n_fft: int, **kwargs) -> db.Bag:
+        '''Override the default extract features to extract a single feature from each file.'''
+        logging.info('Extracting WildlifeAcoustics Descriptors')
+        b = b.map(extract_scalar_features_from_audio, frame_length=frame, hop_length=hop, n_fft=n_fft, **kwargs)
+        return b
+
+    @staticmethod
+    def metadata(ddf: dd.DataFrame) -> dd.DataFrame:
+        meta = pd.concat([ddf.dtypes.loc[:'path'], pd.Series({
+            'recorder model': 'string',
+            'recorder serial': 'string',
+            'timestamp': 'datetime64[ns]',
+        }), ddf.dtypes.loc['path':].iloc[1:]])
+
+        m = pd.DataFrame(columns=meta.index.to_list())
+        m = m.astype(meta.to_dict())
+
+        ddf = ddf.map_partitions(WildlifeAcoustics.filename_metadata, filename_column='path', meta=m)
+
+        return ddf
+
+    @staticmethod
+    def filename_metadata(dataframe: pd.DataFrame, filename_column='path') -> pd.DataFrame:
+        df = dataframe[filename_column].str.split('/', expand=True)
+
+        # try:
+        # Split the file name by '_', the first
+        df['recorder model'] = df.iloc[:, -1].str.split('_', expand=True).iloc[:, 0].str[:3]
+        df['recorder serial'] = df.iloc[:, -1].str.split('_', expand=True).iloc[:, 0].str[3:]
+        df['timestamp'] = pd.to_datetime(df.iloc[:, -1].str.extract(r'(\d{8}_\d{6})'), format='%Y%m%d_%H%M%S.WAV')
+        # except IndexError as e:
+        #     logging.warning(f'Error in processing metadata for {dataframe[filename_column]}')
+        #     df['location'] = ''
+        #     df['timestamp'] = datetime.now()
+
+        # Reassemble
+        return dataframe.loc[:, :filename_column].join(df.loc[:, 'recorder model':'timestamp']).join(
+            dataframe.loc[:, filename_column:].iloc[:, 1:])
+
+    @staticmethod
+    def to_dataframe(b: db.Bag, data_keys: List = None, columns_key=None) -> dd.DataFrame:
+        '''Override the default to_dataframe to return a single row from each bag.'''
+        b = b.map(reformat_for_dataframe, data_keys=data_keys, columns_key=columns_key, scalar_values=True).flatten()
+
+        ddf = b.to_dataframe()
+
+        return ddf
+
+
 datasets = {
     'SoundingOutDiurnal': SoundingOutDiurnal,
     'SoundingOutChorus': SoundingOutChorus,
     'ReefChorus': ReefChorus,
-    'Cairngorms': Cairngorms
+    'Cairngorms': Cairngorms,
+    'WildLifeAcoustics': WildlifeAcoustics,
 }

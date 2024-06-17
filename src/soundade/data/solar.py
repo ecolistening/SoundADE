e53d83b819ccf90ca1518005d6f78aedd9edc437
David Kadish <david.kadish@gmail.com>
2024-06-17 16:36:33 +0200

    Update solar and split to be backwards compatible with python 3.9

--- src/soundade/data/solar.py
+++ src/soundade/data/solar.py
@@ -4,6 +4,11 @@ import pandas as pd
 from astral import LocationInfo
 from astral.sun import sun
 
+<<<<<<< Updated upstream
+=======
+from typing import Union
+
+>>>>>>> Stashed changes
 locations_default = Path(__file__).parent / '../../../data/site_locations.parquet'
 
 def find_sun(r, locations):
@@ -19,7 +24,11 @@ def find_sun(r, locations):
 
 tod_cols = ['dawn', 'sunrise', 'noon', 'sunset', 'dusk']
 
+<<<<<<< Updated upstream
 def solartimes(dataframe: pd.DataFrame, locations: pd.DataFrame | Path | str = locations_default) -> pd.DataFrame:
+=======
+def solartimes(dataframe: pd.DataFrame, locations: Union[pd.DataFrame, Path, str] = locations_default) -> pd.DataFrame:
+>>>>>>> Stashed changes
     """
     Calculate solar event times (dawn, sunrise, sunset, dusk) for each timestamp in the given dataframe.
 
@@ -49,7 +58,11 @@ def solartimes(dataframe: pd.DataFrame, locations: pd.DataFrame | Path | str = l
     # ...
     # ...
     # ...
+<<<<<<< Updated upstream
 def solartimes(dataframe: pd.DataFrame, locations: pd.DataFrame | Path | str = locations_default) -> pd.DataFrame:
+=======
+def solartimes(dataframe: pd.DataFrame, locations: Union[pd.DataFrame, Path, str] = locations_default) -> pd.DataFrame:
+>>>>>>> Stashed changes
 
 
     # print(f'dataframe: {dataframe.shape}')

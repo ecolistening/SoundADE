c6144109e35c428e2c735df6f35e7594885bd8b9
Nic <nrs23@sussex.ac.uk>
2025-03-18 15:34:08 +0000

    [FEATURE] Use explicit library versions, keep a copy of loose defs for development

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export PYTHONPATH="$BASE_PATH/src:$PYTHONPATH"
 cd $BASE_PATH
+conda run -n soundade conda env export -c conda-forge -c anaconda -c defaults > $BASE_PATH/../data/environments/environment-current.yml
 conda run -n soundade xargs -a $PROFILE_PATH python $BASE_PATH/scripts/process_files.py

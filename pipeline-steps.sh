53cd7fbcede866c0b7a80bb7a72d64775a79c7dc
nic <nrs23@sussex.ac.uk>
2025-05-13 15:24:45 +0100

    [CHORE] Update path for processed data

--- pipeline-steps.sh
+++ pipeline-steps.sh
@@ -17,7 +17,7 @@ if [[ ${STEPS_ARRAY[0]} = true ]] ; then
     xargs -a $PROFILE_PATH \
           $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/process_files.py \
           "--indir=$DATA_PATH" \
-          "--outfile=$DATA_PATH/processed.parquet" \
+          "--outfile=$DATA_PATH/processed/processed.parquet" \
           "--cores=$CORES" \
           "--local" \
           "--local_threads=1" \
@@ -27,8 +27,8 @@ fi
 if [[ ${STEPS_ARRAY[1]} = true ]] ; then
     echo "Append metadata"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/append_metadata.py \
-                                         "--infile=$DATA_PATH/processed.parquet" \
-                                         "--outfile=$DATA_PATH/solar.parquet" \
+                                         "--infile=$DATA_PATH/processed/processed.parquet" \
+                                         "--outfile=$DATA_PATH/processed/solar.parquet" \
                                          "--sitesfile=$DATA_PATH/site_locations.parquet" \
                                          "--local" \
                                          -F -C -T \
@@ -39,8 +39,8 @@ fi
 if [[ ${STEPS_ARRAY[2]} = true ]] ; then
     echo "prefilter"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/prefilter.py \
-                                         "--infile=$DATA_PATH/solar.parquet" \
-                                         "--outfile=$DATA_PATH/prefilter.parquet" \
+                                         "--infile=$DATA_PATH/processed/solar.parquet" \
+                                         "--outfile=$DATA_PATH/processed/prefilter.parquet" \
                                          "--local" \
                                          "--cores=$CORES" \
                                          "--memory=$MEM_PER_CPU"
@@ -49,8 +49,8 @@ fi
 if [[ ${STEPS_ARRAY[3]} = true ]] ; then
     echo "to long"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/to_long.py \
-                                         "--infile=$DATA_PATH/prefilter.parquet" \
-                                         "--outfile=$DATA_PATH/to_long.parquet" \
+                                         "--infile=$DATA_PATH/processed/prefilter.parquet" \
+                                         "--outfile=$DATA_PATH/processed/to_long.parquet" \
                                          "--first-frame=3" \
                                          "--local" \
                                          "--cores=$CORES" \
@@ -60,8 +60,8 @@ fi
 if [[ ${STEPS_ARRAY[4]} = true ]] ; then
     echo "summary stats"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/summary_stats.py \
-                                         "--infile=$DATA_PATH/to_long.parquet" \
-                                         "--outfile=$DATA_PATH/summary_stats.parquet" \
+                                         "--infile=$DATA_PATH/processed/to_long.parquet" \
+                                         "--outfile=$DATA_PATH/processed/summary_stats.parquet" \
                                          "--long" \
                                          "--local" \
                                          "--cores=$CORES" \
@@ -71,8 +71,8 @@ fi
 if [[ ${STEPS_ARRAY[5]} = true ]] ; then
     echo "histograms"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/astrograms.py \
-                                         "--infile=$DATA_PATH/to_long.parquet" \
-                                         "--outfile=$DATA_PATH/histograms.parquet" \
+                                         "--infile=$DATA_PATH/processed/to_long.parquet" \
+                                         "--outfile=$DATA_PATH/processed/histograms.parquet" \
                                          "--histogram" \
                                          "--long" \
                                          "--local" \
@@ -83,8 +83,8 @@ fi
 if [[ ${STEPS_ARRAY[6]} = true ]] ; then
     echo "astrograms"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/astrograms.py \
-                                         "--infile=$DATA_PATH/to_long.parquet" \
-                                         "--outfile=$DATA_PATH/astrograms.parquet" \
+                                         "--infile=$DATA_PATH/processed/to_long.parquet" \
+                                         "--outfile=$DATA_PATH/processed/astrograms.parquet" \
                                          "--astrogram" \
                                          "--long" \
                                          "--local" \
@@ -95,8 +95,8 @@ fi
 if [[ ${STEPS_ARRAY[7]} = true ]] ; then
     echo "lz complexity"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/lempel_ziv_complexity.py \
-                                         "--infile=$DATA_PATH/prefilter.parquet" \
-                                         "--outfile=$DATA_PATH/lz_complexity.parquet" \
+                                         "--infile=$DATA_PATH/processed/prefilter.parquet" \
+                                         "--outfile=$DATA_PATH/processed/lz_complexity.parquet" \
                                          "--first-frame=3" \
                                          "--local" \
                                          "--cores=$CORES" \
@@ -111,5 +111,3 @@ if [[ -n "$GIT_COMMIT" ]]; then
 else
     git rev-parse HEAD > $DATA_PATH/run-environment/commit.hash;
 fi
-
-

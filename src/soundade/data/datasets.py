ca2e8aec43abf65b05f9ad021fa50cd984466045
Kieran Gibb <kg@magmacollective.org>
2025-06-11 10:46:04 +0000

    add configs for kilpis

--- src/soundade/data/datasets.py
+++ src/soundade/data/datasets.py
@@ -676,6 +676,71 @@ class WildlifeAcoustics(Dataset):
         return dataframe.loc[:, :filename_column].join(df.loc[:, 'recorder model':'timestamp']).join(
             dataframe.loc[:, filename_column:].iloc[:, 1:])
 
+
+class Kilpisjarvi(Dataset):
+    FILE_PATH_REGEX = r"(?P<location>K\d+)/Data/(?P<recorder>SMA\d+)_(?P<timestamp>\d+_\d+)\.wav"
+
+    @staticmethod
+    def to_dataframe(b: db.Bag, data_keys: List = None, columns_key=None) -> dd.DataFrame:
+        '''Override the default to_dataframe to return a single row from each bag.'''
+        b = b.map(
+            reformat_for_dataframe,
+            data_keys=data_keys,
+            columns_key=columns_key,
+            scalar_values=True
+        ).flatten()
+        ddf = b.to_dataframe()
+        return ddf
+
+    @staticmethod
+    def extract_features(b: db.Bag, frame: int, hop: int, n_fft: int, **kwargs) -> db.Bag:
+        '''Override the default extract features to extract a single feature from each file.'''
+        logging.info('Extracting Kilpisjarvi Features')
+        b = b.map(
+            extract_scalar_features_from_audio,
+            frame_length=frame,
+            hop_length=hop,
+            n_fft=n_fft,
+            **kwargs
+        )
+        return b
+
+    @staticmethod
+    def metadata(ddf: dd.DataFrame) -> dd.DataFrame:
+        import pdb; pdb.set_trace()
+        meta = pd.concat([ddf.dtypes.loc[:'path'], pd.Series({
+            'location': 'string',
+            'timestamp': 'datetime64[ns]',
+        }), ddf.dtypes.loc['path':].iloc[1:]])
+
+        m = pd.DataFrame(columns=meta.index.to_list())
+        m = m.astype(meta.to_dict())
+
+        ddf = ddf.map_partitions(Kilpisjarvi.filename_metadata, filename_column='path', meta=m)
+
+        return ddf
+
+    @staticmethod
+    def filename_metadata(df: pd.DataFrame, filename_column='path') -> pd.DataFrame:
+        import pdb; pdb.set_trace()
+        # meta_raw = re.search(regstr, d['path'].name).groupdict()
+
+        df = df[filename_column].str.extract(self.FILE_PATH_REGEX, expand=True)
+
+        import pdb; pdb.set_trace()
+
+        try:
+            df['location'] = df.iloc[:, -3].str.split('_', expand=True).iloc[:, -1]
+            df['timestamp'] = pd.to_datetime(df.iloc[:, -2], format='%Y%m%d_%H%M%S.WAV')
+        except IndexError as e:
+            logging.warning(f'Error in processing metadata for {df[filename_column]}')
+            df['location'] = ''
+            df['timestamp'] = datetime.now()
+
+        # Reassemble
+        return df.loc[:, :filename_column].join(df.loc[:, 'location':'timestamp']).join(
+            df.loc[:, filename_column:].iloc[:, 1:])
+
     @staticmethod
     def to_dataframe(b: db.Bag, data_keys: List = None, columns_key=None) -> dd.DataFrame:
         '''Override the default to_dataframe to return a single row from each bag.'''
@@ -684,7 +749,6 @@ class WildlifeAcoustics(Dataset):
         ddf = b.to_dataframe()
 
         return ddf
-
 
 datasets = {
     'SoundingOutDiurnal': SoundingOutDiurnal,

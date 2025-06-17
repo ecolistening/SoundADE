f0b5a0e424a32190cabec43a8a8becdef3c58fc6
nic <nrs23@sussex.ac.uk>
2025-06-17 09:37:49 +0100

    [TASK] Make clearer labels for processing stages

--- pipeline-steps.sh
+++ pipeline-steps.sh
@@ -13,7 +13,7 @@ done
 
 cd $CODE_PATH
 if [[ ${STEPS_ARRAY[0]} = true ]] ; then
-    echo "Process files"
+    echo -e "\nProcess audio files\n------"
     xargs -a $PROFILE_PATH \
           $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/process_files.py \
           "--indir=$DATA_PATH" \
@@ -25,7 +25,7 @@ if [[ ${STEPS_ARRAY[0]} = true ]] ; then
 fi
 
 if [[ ${STEPS_ARRAY[1]} = true ]] ; then
-    echo "Append metadata"
+    echo -e "\nAppend solar metadata\n------"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/append_metadata.py \
                                          "--infile=$DATA_PATH/processed/processed.parquet" \
                                          "--outfile=$DATA_PATH/processed/solar.parquet" \
@@ -37,7 +37,7 @@ if [[ ${STEPS_ARRAY[1]} = true ]] ; then
 fi
 
 if [[ ${STEPS_ARRAY[2]} = true ]] ; then
-    echo "prefilter"
+    echo -e "\nPrefilter\n-------"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/prefilter.py \
                                          "--infile=$DATA_PATH/processed/solar.parquet" \
                                          "--outfile=$DATA_PATH/processed/prefilter.parquet" \
@@ -47,7 +47,7 @@ if [[ ${STEPS_ARRAY[2]} = true ]] ; then
 fi
 
 if [[ ${STEPS_ARRAY[3]} = true ]] ; then
-    echo "to long"
+    echo -e "\nConvert to long\n------"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/to_long.py \
                                          "--infile=$DATA_PATH/processed/prefilter.parquet" \
                                          "--outfile=$DATA_PATH/processed/to_long.parquet" \
@@ -58,7 +58,7 @@ if [[ ${STEPS_ARRAY[3]} = true ]] ; then
 fi
 
 if [[ ${STEPS_ARRAY[4]} = true ]] ; then
-    echo "summary stats"
+    echo -e "\nSummary stats\n------"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/summary_stats.py \
                                          "--infile=$DATA_PATH/processed/to_long.parquet" \
                                          "--outfile=$DATA_PATH/processed/summary_stats.parquet" \
@@ -69,7 +69,7 @@ if [[ ${STEPS_ARRAY[4]} = true ]] ; then
 fi
 
 if [[ ${STEPS_ARRAY[5]} = true ]] ; then
-    echo "histograms"
+    echo -e "\nHistograms\n------"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/astrograms.py \
                                          "--infile=$DATA_PATH/processed/to_long.parquet" \
                                          "--outfile=$DATA_PATH/processed/histograms.parquet" \
@@ -81,7 +81,7 @@ if [[ ${STEPS_ARRAY[5]} = true ]] ; then
 fi
 
 if [[ ${STEPS_ARRAY[6]} = true ]] ; then
-    echo "astrograms"
+    echo -e "\nAstrograms\n------"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/astrograms.py \
                                          "--infile=$DATA_PATH/processed/to_long.parquet" \
                                          "--outfile=$DATA_PATH/processed/astrograms.parquet" \
@@ -93,7 +93,7 @@ if [[ ${STEPS_ARRAY[6]} = true ]] ; then
 fi
 
 if [[ ${STEPS_ARRAY[7]} = true ]] ; then
-    echo "lz complexity"
+    echo -e "\nLZ complexity\n-------"
     $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/lempel_ziv_complexity.py \
                                          "--infile=$DATA_PATH/processed/prefilter.parquet" \
                                          "--outfile=$DATA_PATH/processed/lz_complexity.parquet" \

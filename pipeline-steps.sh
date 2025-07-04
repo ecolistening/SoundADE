98e3f0a11c802ac749518960966d4f421a1e55fd
nic <nrs23@sussex.ac.uk>
2025-07-04 09:17:27 +0100

    [CHANGE] Define/execute pipeline steps as comma separated list

--- pipeline-steps.sh
+++ pipeline-steps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONDA_PATH=/opt/conda
 
-IFS=':' read -ra STEPS_ARRAY <<< "$STEPS"
+IFS=',' read -ra STEPS_ARRAY <<< "$STEPS"
 
 # Option to use local conda environments
 while getopts "l" flag; do
@@ -39,109 +39,115 @@ set_run_environment () {
 }
 
 cd $CODE_PATH
-if [[ ${STEPS_ARRAY[0]} = true ]] ; then
-    echo -e "\nProcess audio files\n------"
-    set_run_environment "process_files"
-    xargs -a $PROFILE_PATH \
-          $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/process_files.py \
-          "--indir=$DATA_PATH" \
-          "--outfile=$DATA_PATH/processed/processed.parquet" \
-          "--cores=$CORES" \
-          "--local" \
-          "--local_threads=1" \
-          "--memory=$MEM_PER_CPU" \
-          > $STEP_LOG 2>&1
-fi
+for STEP in "${STEPS_ARRAY[@]}"; do
+    if [[ $STEP -eq 0 ]] ; then
+        echo -e "\nProcess audio files\n------"
+        set_run_environment "process_files"
+        xargs -a $PROFILE_PATH \
+            $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/process_files.py \
+            "--indir=$DATA_PATH" \
+            "--outfile=$DATA_PATH/processed/processed.parquet" \
+            "--cores=$CORES" \
+            "--local" \
+            "--local_threads=1" \
+            "--memory=$MEM_PER_CPU" \
+            > $STEP_LOG 2>&1
+    fi
 
-if [[ ${STEPS_ARRAY[1]} = true ]] ; then
-    echo -e "\nAppend solar metadata\n------"
-    set_run_environment "append_metadata"
-    $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/append_metadata.py \
-                                         "--infile=$DATA_PATH/processed/processed.parquet" \
-                                         "--outfile=$DATA_PATH/processed/solar.parquet" \
-                                         "--sitesfile=$DATA_PATH/site_locations.parquet" \
-                                         "--local" \
-                                         -F -C -T \
-                                         "--cores=$CORES" \
-                                         "--memory=$MEM_PER_CPU" \
-    > $STEP_LOG 2>&1
-fi
+    if [[ $STEP -eq 1 ]] ; then
+        echo -e "\nAppend solar metadata\n------"
+        set_run_environment "append_metadata"
+        $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/append_metadata.py \
+                                            "--infile=$DATA_PATH/processed/processed.parquet" \
+                                            "--outfile=$DATA_PATH/processed/solar.parquet" \
+                                            "--sitesfile=$DATA_PATH/site_locations.parquet" \
+                                            "--local" \
+                                            -F -C -T \
+                                            "--cores=$CORES" \
+                                            "--memory=$MEM_PER_CPU" \
+        > $STEP_LOG 2>&1
+    fi
 
-if [[ ${STEPS_ARRAY[2]} = true ]] ; then
-    echo -e "\nPrefilter\n-------"
-    set_run_environment "prefilter"
-    $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/prefilter.py \
-                                         "--infile=$DATA_PATH/processed/solar.parquet" \
-                                         "--outfile=$DATA_PATH/processed/prefilter.parquet" \
-                                         "--local" \
-                                         "--cores=$CORES" \
-                                         "--memory=$MEM_PER_CPU" \
-    > $STEP_LOG 2>&1
-fi
+    if [[ $STEP -eq 2 ]] ; then
+        echo -e "\nPrefilter\n-------"
+        set_run_environment "prefilter"
+        $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/prefilter.py \
+                                            "--infile=$DATA_PATH/processed/solar.parquet" \
+                                            "--outfile=$DATA_PATH/processed/prefilter.parquet" \
+                                            "--local" \
+                                            "--cores=$CORES" \
+                                            "--memory=$MEM_PER_CPU" \
+        > $STEP_LOG 2>&1
+    fi
 
-if [[ ${STEPS_ARRAY[3]} = true ]] ; then
-    echo -e "\nConvert to long\n------"
-    set_run_environment "to_long"
-    $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/to_long.py \
-                                         "--infile=$DATA_PATH/processed/prefilter.parquet" \
-                                         "--outfile=$DATA_PATH/processed/to_long.parquet" \
-                                         "--first-frame=3" \
-                                         "--local" \
-                                         "--cores=$CORES" \
-                                         "--memory=$MEM_PER_CPU" \
-    > $STEP_LOG 2>&1
-fi
+    if [[ $STEP -eq 3 ]] ; then
+        echo -e "\nConvert to long\n------"
+        set_run_environment "to_long"
+        $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/to_long.py \
+                                            "--infile=$DATA_PATH/processed/prefilter.parquet" \
+                                            "--outfile=$DATA_PATH/processed/to_long.parquet" \
+                                            "--first-frame=3" \
+                                            "--local" \
+                                            "--cores=$CORES" \
+                                            "--memory=$MEM_PER_CPU" \
+        > $STEP_LOG 2>&1
+    fi
 
-if [[ ${STEPS_ARRAY[4]} = true ]] ; then
-    echo -e "\nSummary stats\n------"
-    set_run_environment "summary_stats"
-    $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/summary_stats.py \
-                                         "--infile=$DATA_PATH/processed/to_long.parquet" \
-                                         "--outfile=$DATA_PATH/processed/summary_stats.parquet" \
-                                         "--long" \
-                                         "--local" \
-                                         "--cores=$CORES" \
-                                         "--memory=$MEM_PER_CPU" \
-    > $STEP_LOG 2>&1
-fi
+    if [[ $STEP -eq 4 ]] ; then
+        echo -e "\nSummary stats\n------"
+        set_run_environment "summary_stats"
+        $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/summary_stats.py \
+                                            "--infile=$DATA_PATH/processed/to_long.parquet" \
+                                            "--outfile=$DATA_PATH/processed/summary_stats.parquet" \
+                                            "--long" \
+                                            "--local" \
+                                            "--cores=$CORES" \
+                                            "--memory=$MEM_PER_CPU" \
+        > $STEP_LOG 2>&1
+    fi
 
-if [[ ${STEPS_ARRAY[5]} = true ]] ; then
-    echo -e "\nHistograms\n------"
-    set_run_environment "histograms"
-    $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/astrograms.py \
-                                         "--infile=$DATA_PATH/processed/to_long.parquet" \
-                                         "--outfile=$DATA_PATH/processed/histograms.parquet" \
-                                         "--histogram" \
-                                         "--long" \
-                                         "--local" \
-                                         "--cores=$CORES" \
-                                         "--memory=$MEM_PER_CPU" \
-    > $STEP_LOG 2>&1
-fi
+    if [[ $STEP -eq 5 ]] ; then
+        echo -e "\nHistograms\n------"
+        set_run_environment "histograms"
+        $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/astrograms.py \
+                                            "--infile=$DATA_PATH/processed/to_long.parquet" \
+                                            "--outfile=$DATA_PATH/processed/histograms.parquet" \
+                                            "--histogram" \
+                                            "--long" \
+                                            "--local" \
+                                            "--cores=$CORES" \
+                                            "--memory=$MEM_PER_CPU" \
+        > $STEP_LOG 2>&1
+    fi
 
-if [[ ${STEPS_ARRAY[6]} = true ]] ; then
-    echo -e "\nAstrograms\n------"
-    set_run_environment "astrograms"
-    $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/astrograms.py \
-                                         "--infile=$DATA_PATH/processed/to_long.parquet" \
-                                         "--outfile=$DATA_PATH/processed/astrograms.parquet" \
-                                         "--astrogram" \
-                                         "--long" \
-                                         "--local" \
-                                         "--cores=$CORES" \
-                                         "--memory=$MEM_PER_CPU" \
-    > $STEP_LOG 2>&1
-fi
+    if [[ $STEP -eq 6 ]] ; then
+        echo -e "\nAstrograms\n------"
+        set_run_environment "astrograms"
+        $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/astrograms.py \
+                                            "--infile=$DATA_PATH/processed/to_long.parquet" \
+                                            "--outfile=$DATA_PATH/processed/astrograms.parquet" \
+                                            "--astrogram" \
+                                            "--long" \
+                                            "--local" \
+                                            "--cores=$CORES" \
+                                            "--memory=$MEM_PER_CPU" \
+        > $STEP_LOG 2>&1
+    fi
 
-if [[ ${STEPS_ARRAY[7]} = true ]] ; then
-    echo -e "\nLZ complexity\n-------"
-    set_run_environment "lz_complexity"
-    $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/lempel_ziv_complexity.py \
-                                         "--infile=$DATA_PATH/processed/prefilter.parquet" \
-                                         "--outfile=$DATA_PATH/processed/lz_complexity.parquet" \
-                                         "--first-frame=3" \
-                                         "--local" \
-                                         "--cores=$CORES" \
-                                         "--memory=$MEM_PER_CPU" \
-    > $STEP_LOG 2>&1
-fi
+    if [[ $STEP -eq 7 ]] ; then
+        echo -e "\nLZ complexity\n-------"
+        set_run_environment "lz_complexity"
+        $CONDA_PATH/envs/soundade/bin/python $CODE_PATH/scripts/lempel_ziv_complexity.py \
+                                            "--infile=$DATA_PATH/processed/prefilter.parquet" \
+                                            "--outfile=$DATA_PATH/processed/lz_complexity.parquet" \
+                                            "--first-frame=3" \
+                                            "--local" \
+                                            "--cores=$CORES" \
+                                            "--memory=$MEM_PER_CPU" \
+        > $STEP_LOG 2>&1
+    fi
+done
+
+# Remove unneeded copies of run-environment
+rm $DATA_PATH/processed/run-environment/profile
+rm $DATA_PATH/processed/run-environment/settings.env

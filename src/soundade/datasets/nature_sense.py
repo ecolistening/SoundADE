9ec0399e6c3065e0c79a9350323d2e161daa5d5b
Kieran Gibb <kg@magmacollective.org>
2025-08-01 10:19:30 +0100

    use regex for simplified folder structure

--- src/soundade/datasets/nature_sense.py
+++ src/soundade/datasets/nature_sense.py
@@ -4,11 +4,12 @@ from soundade.datasets.base import Dataset
 @dataclass
 class NatureSense(Dataset):
     PATTERN = (
-        "(?P<recorder_model>Audiomoths|Song_Meter_Mini_2|SM4_Bat)/"
-        "(?P<site_level_1>[^/]+)/"
-        "[^/_]+_(?P<site_level_2>[^/_]+(?:[ _ ][^/_]+)*?)(?:_\d{8})?/"
-        "(?:[^_/]+_)?"
-        "(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})_"
-        "(?P<hour>\d{2})(?P<minute>\d{2})(?P<second>\d{2})"
-        "\.(?:wav|WAV|flac|FLAC|mp3|MP3)"
+        r"(?P<recorder_model>Audiomoths|Song_Meter_Mini_2|SM4_Bat)/"
+        r"(?P<site_level_1>[^/]+)/"
+        r"(?P<site_level_2>[^/]+)/"
+        r"(?:\d{8}/)?"
+        r"(?P<device_id>[^/_]+)_"
+        r"(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})_"
+        r"(?P<hour>\d{2})(?P<minute>\d{2})(?P<second>\d{2})"
+        r"\.(?i:wav|flac|mp3)"
     )
